84290a2bb9ef7215b8ca8268cacfd11fb060367c
Matteo Aloisi <99matteoaloisi@gmail.com>
2022-10-15 13:06:14 +0200

    Aggiunti alcuni commenti per la comprensibilità del codice

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ let elementiNavAdattata = document.querySelectorAll("#elemento-nav-adattata");
 
 //Inizializzazione delle funzioni
 
-function navPriority () {
+function navPriority () {       //Funziona pensata per dare "priorità" alla navbar (il resto dello schermo verrà offuscato)
     
 elementiNavAdattata.forEach(element => {
-    element.addEventListener("mouseover", () => {
-        if (element == elementiNavAdattata[0]) { }
+    element.addEventListener("mouseover", () => {   //Aggiunge l'evento per ogni elemento presente nella navbar in basso (adattata)
+        if (element == elementiNavAdattata[0]) { }      //Se ricade sul contenitore trade non fa nulla
         else {offuscaSchermo.classList.add("offusca-schermo-on");
             offuscaSchermo.classList.remove("offusca-schermo-off");}
     });
